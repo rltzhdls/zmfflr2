50517e405513fbac2cc3ae3ee7bc9b97d826c812
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-18 13:32:31 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineageM/update/211110/oasis_sub_C.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
